6a51cd53540d6b0035c188bf4bf8feeddd7846e7
Mohamed Mahmoud Ahmed Mahmoud ELGendy <109443070+medo295@users.noreply.github.com>
2025-03-19 23:27:01 +0200

    Create mmm.cpp

---
+++ mmm.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define M ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+int main() {
+#include <iostream>
+    using namespace std;
+    int main() {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        long long  difference = (a*b)-(c*d);
+        cout<<"Difference = "<<difference<<'\n';
+    }
+}
