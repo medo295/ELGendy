f084bd77c4ef62a3ab3999a6f278102a18cf2347
Mohamed Mahmoud Ahmed Mahmoud ELGendy <109443070+medo295@users.noreply.github.com>
2025-04-30 16:35:08 +0300

    Update Pointer.cpp

--- Pointer.cpp
+++ Pointer.cpp
@@ -19,4 +19,19 @@ second
     double *dp=(double*)p; // valid
     // if store void pointer  in pointer has datatype as double you do (*need*) to cast
 ===========================================================================================
-
+Third
+    float* ptr,data;
+    // ptr is pointer , data is normal variable
+    float *ptr,data;
+    // ptr is pointer , data is normal variable
+===========================================================================================
+Fourth
+    (is valid)
+    int n=120; 
+    int *ptr;  
+    ptr = &n;
+    =========
+    (Not Valid)
+    int *ptr;
+    ptr = &120;
+===========================================================================================
